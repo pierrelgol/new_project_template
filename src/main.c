70a1ad13c1b20302cb86895eec7ef630326919c4
PierreLgol <119697612+pierrelgol@users.noreply.github.com>
2023-04-13 23:52:55 +0200

    template

---
+++ src/main.c
@@ -0,0 +1,51 @@
+/*
+ *
+ *   Title of project : 
+ *   Date of creation : 
+ *   Author(s) : PierreLgol
+ *   github : https://github.com/pierrelgol
+ * 
+ *   More on the project :
+ *   
+ *   #TODO(s) :  
+ * 
+ *
+ */
+
+//--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//--------------------[     CONSTANT DEFINITION     ]--------------------//
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+//--------------------[     STRUCT DEFINITION     ]--------------------//
+
+
+
+//--------------------[     FUNCTION DEFINITION     ]--------------------//
+
+
+
+//--------------------[     MAIN METHOD     ]--------------------//
+
+int main()
+{
+
+
+return EXIT_SUCCESS;
+}
+
+//--------------------------------------------------------------------------//
+
+
+
+//--------------------[     FUNCTION IMPLEMENTATION     ]--------------------//
+
+
+
+//--------------------------------------------------------------------------//
